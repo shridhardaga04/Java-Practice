6da0ab042c98fb4abd9424dde1238ce38083ce4a
shridhardaga04 <shridhardaga04@gmail.com>
2022-05-13 12:46:50 +0530

    Immutability for StringBuilder

--- src/ImmutableDemo.java
+++ src/ImmutableDemo.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class ImmutableDemo {
 
     public static void main(String[] args) {
-        ImmutablePerson p1 = new ImmutablePerson("ddd", 22, new ArrayList<>());
+        ImmutablePerson p1 = new ImmutablePerson("ddd", 22, new ArrayList<>(), new StringBuilder("lastName"));
         List<Address> ad1 = p1.getAd();
         ad1.add(new Address());
     }
@@ -18,12 +18,14 @@ public class ImmutableDemo {
 
 final class ImmutablePerson{
     private final String name;
+    private final StringBuilder lastName;
     private final int age;
     private final List<Address> ad = new ArrayList<>();
 
-    public ImmutablePerson(String name, int age, List<Address> ad) {
+    public ImmutablePerson(String name, int age, List<Address> ad, StringBuilder lastName) {
         this.name = name;
         this.age = age;
+        this.lastName = lastName;
         Collections.copy(this.ad, ad);
     }
 
@@ -34,6 +36,10 @@ final class ImmutablePerson{
     public int getAge() {
         return age;
     }
+
+    public String getLastName() {
+        return lastName.toString();
+    } // While dealing with StringBuilder we can return SB by converting to String so that immutability can be achieved
 
     public List<Address> getAd() {
         return Collections.unmodifiableList(this.ad);
